f314f1a3bb12afbe24f0bb27672c7a27d88f865d
AndyMalcolm <ayhrunin.fitu21@kubg.edu.ua>
2023-08-21 01:21:55 +0300

    test

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+// import { galleryItems } from './gallery-items.js';
+// // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+import { galleryItems } from './gallery-items.js';
+
+const gallery = document.querySelector('.gallery');
+
+function createGalleryItem(item) {
+  const galleryItem = document.createElement('li');
+  galleryItem.classList.add('gallery__item');
+
+  const link = document.createElement('a');
+  link.classList.add('gallery__link');
+  link.href = item.original;
+
+  const image = document.createElement('img');
+  image.classList.add('gallery__image');
+  image.src = item.preview;
+  image.setAttribute('data-source', item.original);
+  image.alt = item.description;
+
+  link.appendChild(image);
+  galleryItem.appendChild(link);
+
+  return galleryItem;
+}
+
+const galleryItemsMarkup = galleryItems.map(item => createGalleryItem(item));
+gallery.append(...galleryItemsMarkup);
+
+gallery.addEventListener('click', e => {
+  e.preventDefault();
+  if (e.target.classList.contains('gallery__image')) {
+    const source = e.target.getAttribute('data-source');
+    const instance = basicLightbox.create(`
+      <img src="${source}" alt="${e.target.alt}">
+    `);
+    instance.show();
+  }
+});
