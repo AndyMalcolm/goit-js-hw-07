ffa65bf358b8ed2227a5be2df73e599666f91863
AndyMalcolm <ayhrunin.fitu21@kubg.edu.ua>
2023-08-24 00:37:42 +0300

    найс

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,41 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+// кoд с 1 задания
+import { galleryItems } from './gallery-items.js';
+const gallery = document.querySelector('.gallery');
+
+function createGalleryItem(item) {
+  const galleryItem = document.createElement('li');
+  galleryItem.classList.add('gallery__item');
+
+  const link = document.createElement('a');
+  link.classList.add('gallery__link');
+  link.href = item.original;
+
+  const image = document.createElement('img');
+  image.classList.add('gallery__image');
+  image.src = item.preview;
+  image.setAttribute('data-source', item.original);
+  image.alt = item.description;
+
+  link.appendChild(image);
+  galleryItem.appendChild(link);
+
+  return galleryItem;
+}
+
+const galleryItemsMarkup = galleryItems.map(item => createGalleryItem(item));
+gallery.append(...galleryItemsMarkup);
+
+gallery.addEventListener('click', e => {
+  e.preventDefault();
+  if (e.target.classList.contains('gallery__image')) {
+    const source = e.target.dataset.source;//e.target.getAttribute('data-source')
+    basicLightbox.create(`
+<img width= "1400" height= "900" src= "${source}">`)
+.show();
+  }
+});
+console.log(galleryItems);
