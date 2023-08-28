9c585901fbc949d23f411ad7c3bfd75b1b860e5a
AndyMalcolm <ayhrunin.fitu21@kubg.edu.ua>
2023-08-28 11:39:55 +0300

    hm

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -33,3 +33,15 @@ const lightbox = new SimpleLightbox('.gallery a.gallery__link', {
   captionsData: "alt",
   captionDelay: 250,
 });
+
+const galleryHtml = `
+  <ul class="gallery">
+    ${galleryItems.map(item => `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+          <img class="gallery__image" src="${item.preview}" alt="${item.description}" data-source="${item.original}" />
+        </a>
+      </li>
+    `).join('')}
+  </ul>
+`;
